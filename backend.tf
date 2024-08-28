318427935230e3b89d7e3203ca85f16794da3518
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-08-28 18:41:49 +0530

    commented backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "knrajan"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "dbstatelock"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "knrajan"
+#     key            = "terraform.tfstate"
+#     region         = "us-east-1"
+#     encrypt        = true
+#     dynamodb_table = "dbstatelock"
+#   }
+# }
