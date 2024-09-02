2fa0b18038e3ed3f192bb0ea92d2d32b8c3a25b3
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-09-02 16:18:19 +0530

    Creating an ec2 with userdefined SG

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "newinstance" {
   ami                    = "ami-0e86e20dae9224db8"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.ddsl-securitygroup.id]  
+  #vpc_security_group_ids = [aws_security_group.ddsl-securitygroup.id]    
 
   tags = {
     Name        = "newinstance"
